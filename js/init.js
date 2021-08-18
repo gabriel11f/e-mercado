f9ccca44127b9aecbcfc8264dc134f16c168555a
Gabriel Fernández <fernandezjosegabriel02@gmail.com>
2021-08-18 02:45:07 -0300

    css for login
    addedd css for login and started working on the login page as a front page if user is not logged in.

--- js/init.js
+++ js/init.js
@@ -43,5 +43,6 @@ var getJSONData = function(url){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", login => {
+  window.location.sessionStorage = 'login.html';
 });
