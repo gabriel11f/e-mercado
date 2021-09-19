ac4c0c2b622001493f02d8b76be11c5ca4936342
Gabriel Fernández <fernandezjosegabriel02@gmail.com>
2021-09-19 19:42:26 -0300

    cambios entrega 3

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,7 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
 var prodInfo = {};
 
 function showImagesGallery(array){
@@ -18,8 +19,23 @@ function showImagesGallery(array){
         </div>
         `
 
-        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+        document.getElementById("ImagesGallery").innerHTML = htmlContentToAppend;
     }
+}
+
+const showFullData = (array) => {
+     for (let valor of array) {
+         document.getElementById("comments").innerHTML += 
+         '<h4>' +  valor.user + '</h4> <br><p>Valoración: ' + valor.score + '  </p>' + '<br> <p>' + valor.description +'</p> <hr><hr>'
+
+    }
+}
+
+const showRatingNComments = () => {
+    fetch(PRODUCT_INFO_COMMENTS_URL)
+    .then(data => data.json())
+    .then(data => {
+        showFullData(data)})
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -30,20 +46,26 @@ document.addEventListener("DOMContentLoaded", function(e){
         if (resultObj.status === "ok")
         {
             prodInfo = resultObj.data;
+        
 
+            let productCategoryHTML = document.getElementById("productCategory");
             let prodInfoNameHTML  = document.getElementById("productName");
             let prodInfoDescriptionHTML = document.getElementById("productDescription");
             let productsoldCountHTML = document.getElementById("productsoldCount");
             let productCostHTML = document.getElementById("productCost");
+
         
+            productCategoryHTML.innerHTML = prodInfo.category;
             prodInfoNameHTML.innerHTML = prodInfo.name;
             prodInfoDescriptionHTML.innerHTML = prodInfo.description;
             productsoldCountHTML.innerHTML = prodInfo.soldCount;
             productCostHTML.innerHTML = prodInfo.currency + " " + prodInfo.cost;
+
             
 
             //Muestro las imagenes en forma de galería
             showImagesGallery(prodInfo.images);
+            showRatingNComments()
+
         }
-    });
-});
+    })})
